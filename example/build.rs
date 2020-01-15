fce4b488dcc0d493b67c592d74e76317a19878be
Hui Zhu <teawater@gmail.com>
2020-01-15 17:18:35 +0800

    Fix tab format issue
    Signed-off-by: Hui Zhu <teawater@antfin.com>

--- example/build.rs
+++ example/build.rs
@@ -1,9 +1,9 @@
 use cmd_lib::run_cmd;
 
 fn main() {
-  let result = run_cmd! {
-    cd protocols;
-    ./hack/update-generated-proto.sh;
-  };
-  result.unwrap();
+    let result = run_cmd! {
+      cd protocols;
+      ./hack/update-generated-proto.sh;
+    };
+    result.unwrap();
 }
