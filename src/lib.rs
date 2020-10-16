b275731933abf79744c6ed9e09214ae3d3064bf2
Tim Zhang <tim@hyper.sh>
2020-10-16 13:28:29 +0800

    Export get_rpc_status as ttrpc::get_rpc_error
    And make get_rpc_status and get_status  accept Into<String> as msg.
    
    Signed-off-by: Tim Zhang <tim@hyper.sh>

--- src/lib.rs
+++ src/lib.rs
@@ -45,7 +45,7 @@ pub use compiled::ttrpc;
 #[doc(inline)]
 pub use crate::common::MessageHeader;
 #[doc(inline)]
-pub use crate::error::{get_status, Error, Result};
+pub use crate::error::{get_rpc_status as get_rpc_error, get_status, Error, Result};
 #[doc(inline)]
 pub use crate::ttrpc::{Code, Request, Response, Status};
 
