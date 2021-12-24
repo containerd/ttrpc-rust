c1b54987a9903a68ccb0284f5c664fe853985b25
Tim Zhang <tim@hyper.sh>
2021-12-25 00:07:54 +0800

    refactor: remove Domain::AbstractUnix
    Domain::AbstractUnix and Domain::Unix are almost same
    execpt that they are slightly different when making address.
    
    Both of them can be represented by Domain::Unix and we left
    '@' prefix in the address after parse_addr, use this prefix
    to determine whether it is abstract.
    
    So that the code is simplified.
    
    Signed-off-by: Tim Zhang <tim@hyper.sh>

--- src/common.rs
+++ src/common.rs
@@ -18,8 +18,6 @@ use std::os::unix::io::RawFd;
 pub(crate) enum Domain {
     Unix,
     #[cfg(target_os = "linux")]
-    AbstractUnix,
-    #[cfg(target_os = "linux")]
     Vsock,
 }
 
@@ -43,25 +41,31 @@ pub(crate) fn do_listen(listener: RawFd) -> Result<()> {
     listen(listener, 10).map_err(|e| Error::Socket(e.to_string()))
 }
 
-pub fn parse_sockaddr(host: &str) -> Result<(Domain, &str)> {
-    if let Some(addr) = host.strip_prefix("unix://") {
-        if !addr.starts_with('@') {
-            return Ok((Domain::Unix, addr));
-        }
+#[cfg(target_os = "linux")]
+fn parse_sockaddr(addr: &str) -> Result<(Domain, &str)> {
+    if let Some(addr) = addr.strip_prefix("unix://") {
+        return Ok((Domain::Unix, addr));
     }
 
-    #[cfg(target_os = "linux")]
-    {
-        if let Some(addr) = host.strip_prefix("unix://@") {
-            return Ok((Domain::AbstractUnix, addr));
-        }
-
-        if let Some(addr) = host.strip_prefix("vsock://") {
-            return Ok((Domain::Vsock, addr));
-        }
+    if let Some(addr) = addr.strip_prefix("vsock://") {
+        return Ok((Domain::Vsock, addr));
     }
 
-    Err(Error::Others(format!("Scheme {:?} is not supported", host)))
+    Err(Error::Others(format!("Scheme {:?} is not supported", addr)))
+}
+
+#[cfg(not(target_os = "linux"))]
+fn parse_sockaddr(addr: &str) -> Result<(Domain, &str)> {
+    if let Some(addr) = addr.strip_prefix("unix://") {
+        if addr.starts_with('@') {
+            return Err(Error::Others(
+                "Abstract unix domain socket is not support on this platform".to_string(),
+            ));
+        }
+        return Ok((Domain::Unix, addr));
+    }
+
+    Err(Error::Others(format!("Scheme {:?} is not supported", addr)))
 }
 
 #[cfg(any(feature = "async", not(target_os = "linux")))]
@@ -84,9 +88,12 @@ pub(crate) const SOCK_CLOEXEC: SockFlag = SockFlag::empty();
 #[cfg(target_os = "linux")]
 fn make_addr(domain: Domain, sockaddr: &str) -> Result<UnixAddr> {
     match domain {
-        Domain::Unix => UnixAddr::new(sockaddr).map_err(err_to_others_err!(e, "")),
-        Domain::AbstractUnix => {
-            UnixAddr::new_abstract(sockaddr.as_bytes()).map_err(err_to_others_err!(e, ""))
+        Domain::Unix => {
+            if let Some(sockaddr) = sockaddr.strip_prefix('@') {
+                UnixAddr::new_abstract(sockaddr.as_bytes()).map_err(err_to_others_err!(e, ""))
+            } else {
+                UnixAddr::new(sockaddr).map_err(err_to_others_err!(e, ""))
+            }
         }
         Domain::Vsock => Err(Error::Others(
             "function make_addr does not support create vsock socket".to_string(),
@@ -118,8 +125,6 @@ fn make_socket(addr: (&str, u32)) -> Result<(RawFd, Domain, SockAddr)> {
 
     let (fd, sockaddr) = match domain {
         Domain::Unix => get_sock_addr(domain, sockaddrv)?,
-        #[cfg(target_os = "linux")]
-        Domain::AbstractUnix => get_sock_addr(domain, sockaddrv)?,
         #[cfg(target_os = "linux")]
         Domain::Vsock => {
             let sockaddr_port_v: Vec<&str> = sockaddrv.split(':').collect();
@@ -221,8 +226,8 @@ mod tests {
             ("Vsock://8:1025", Some(Domain::Vsock), "8:1025", false),
             (
                 "unix://@/run/b.sock",
-                Some(Domain::AbstractUnix),
-                "/run/b.sock",
+                Some(Domain::Unix),
+                "@/run/b.sock",
                 true,
             ),
             ("abc:///run/c.sock", None, "", false),
