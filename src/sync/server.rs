1b915e2e2559ac518a46839a95abda191e49f4db
Quanwei Zhou <quanweiZhou@linux.alibaba.com>
2024-09-23 16:09:00 +0800

    server: fix server exit once a accept failed
    If the Accept error occurs, an error can be output to ensure that the
    subsequent connect can be accepted normally.
    
    Fixes: #239
    Signed-off-by: Quanwei Zhou <quanweiZhou@linux.alibaba.com>

--- src/sync/server.rs
+++ src/sync/server.rs
@@ -441,7 +441,7 @@ impl Server {
                         Ok(fd) => fd,
                         Err(e) => {
                             error!("failed to accept error {:?}", e);
-                            break;
+                            continue;
                         }
                     };
 
@@ -453,13 +453,13 @@ impl Server {
                         Ok(fd) => {
                             if let Err(err) = set_fd_close_exec(fd) {
                                 error!("fcntl failed after accept: {:?}", err);
-                                break;
+                                continue;
                             };
                             fd
                         }
                         Err(e) => {
                             error!("failed to accept error {:?}", e);
-                            break;
+                            continue;
                         }
                     };
 
